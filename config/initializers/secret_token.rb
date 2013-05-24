fa88e71952b148957a1f496bf54315bc658a8806
Zehnab Afzal <zyafzal@dundee.ac.uk>
2013-05-24 15:53:09 +0100

    added product model to store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminTest::Application.config.secret_token = 'a9a51ad3ce6e243e051b219c86a90b1c3be3458e3173f968b4a7e4ab80a036b056e55350ea38c097d904437e6e48ea9af4f12071b0372bffb7661aea8639788e'
